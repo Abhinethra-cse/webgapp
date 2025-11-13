a3f129e50fcc414a7e073887ac3c46c41185fc52
Abhinethra-cse <abhinethraa.cse2024@citchennai.net>
2025-11-13 21:04:29 +0530

    13.11.2025
    1.print string in reverse order
    2.generate bill amount with discount
    3.username and password login error
    4.current bill based on units
    5.email error
    6.count and print positive and negative number from the list
    7.print random number from  1 to 100
    8.loading before printing the result
    9.print list of natural number in reverse order
    10. generate random pin

---
+++ 13.11.2025/task1.py
@@ -0,0 +1,92 @@
+# string="abhi nethra alwar"
+# reverse_string=string[::-1]
+# print("reversed string:",reverse_string)
+
+
+# def bill():
+#     amount = int(input("Enter amount: "))
+#     if amount > 1000:
+#         discount = amount * 0.1
+#         final_amount = amount - discount
+#         print("Final amount after 10% discount:", final_amount)
+#     else:
+#         print("No discount. Final amount:", amount)
+
+# bill()
+
+
+# username=input("username:")
+# password=input("password:")
+# if username=="abhi" and password =="1086":
+#     print("login successful")
+# else:
+#     print("error, incorrect username or password")
+
+
+
+# def current_bill(units):
+#     if units > 100:
+#         current_bill=units*5
+#     else:
+#         current_bill=units*3
+#     return current_bill
+# print("current bill:",current_bill(50))
+
+
+# def email_check(email):
+#         if "@" in email and "." in email:
+#             print("valid email")
+#         else:
+#             print("invalid email, please re-enter")
+# email=input("enter your email:")
+# email_check(email)
+
+
+
+# list=[10,-23,3,-24,45,-89]
+# positive_number=[]
+# negative_number=[]
+# count_pos=0
+# count_neg=0
+# for i in list:
+#     if i>=0:
+#         positive_number.append(i)
+#         count_pos+=1
+#     else:
+#         negative_number.append(i)
+#         count_neg+=1
+# print("positive numbers:",positive_number)
+# print("count of positive numbers:",count_pos)
+# print("negative numbers:",negative_number)
+# print("count of negative numbers:",count_neg)
+
+
+
+# def random_number():
+#     import random
+#     number=random.randint(1,100)
+#     return number
+# print("random number:",random_number())
+
+
+# import time   
+
+# num = int(input("Enter a number: "))
+
+# for i in range(num):
+#     print("Loading...")
+#     time.sleep(1)   
+#     print("Number:", i)
+
+
+# my_list = [1, 2, 3, 4, 5]
+# my_list.reverse()
+# print(my_list)
+
+
+# def pin_generator():
+#     import random
+#     pin = random.randint(1000, 9999)
+#     return pin
+# print("Generated PIN:", pin_generator())
+
